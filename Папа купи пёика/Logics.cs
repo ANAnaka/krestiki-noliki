f66a7d5cec666c99ea49d6b3a15b6fc0ef669964
ANAnaka <112645412+ANAnaka@users.noreply.github.com>
2023-02-03 18:30:18 +0300

    Add files via upload

---
+++ Папа купи пёика/Logics.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Папа_купи_пёика
+{
+    internal class Logics
+    {
+        int[,] map = new int[3, 3]; // 0 - пустая; 1 - крестик; 2 - нолик
+        public int side { get; private set; } // 1 - крестик; 2 - нолик
+        bool play;
+        int step;
+        public string finish { get; private set; }
+        public Logics() 
+        {
+            Init();
+            
+        }
+
+        public void Init()
+        {
+            for (int x=0; x<3; x++)
+            {
+                for (int y=0; y<3; y++)
+                {
+                    map[x, y] = 0;
+                }
+            }
+            side= 1;
+            play = true;
+            step = 0;
+        }
+        public bool Place(int x, int y)
+        {
+            if (!play)
+            {
+                return false;
+            }
+            if (x < 0 || x > 2)
+            {
+                return false;
+            }
+            if (y < 0 || y > 2)
+            {
+                return false;
+            }
+            if (map[x, y] > 0)
+            {
+                return false;
+            }
+            step++;
+            map[x, y] = side;
+            finish = Finish(x,y);
+            
+            if (side == 1)
+            {
+                side = 2;
+            } else
+            {
+                side= 1;
+            }
+                return true;
+        }
+        private string Finish (int x, int y)
+        {
+            bool vin = false;
+            if (map[x, 0] == side && map[x,1] == side && map[x,2] == side)
+            {
+                vin = true;
+            }
+            if (map[0, y] == side && map[1, y] == side && map[2, y] == side)
+            {
+                vin = true;
+            }
+            if (map[0,0] == side && map[1,1] == side && map[2, 2] == side)
+            {
+                vin = true;
+            }
+            if (map[2, 0] == side && map[1, 1] == side && map[0, 2] == side)
+            {
+                vin = true;
+            }
+            if (vin)
+            {
+                if (side == 1)
+                {
+                    play = false;
+                    return "Winx";
+                }
+                else
+                {
+                    play = false;
+                    return "Wino";
+                }     
+            }
+            else
+            {
+                if (step == 9)
+                {
+                    play = false;
+                    return "draw";
+                }
+                else
+                {
+                    return "play";
+                }
+            }
+        }
+    }
+}
---
+++ Папа купи пёика/MainWindow.xaml.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Папа_купи_пёика
+{
+    
+    public partial class MainWindow : Window
+    {
+        Logics logic = new Logics();
+        public MainWindow()
+        {
+            InitializeComponent();
+            block();
+        }
+        private void block()
+        {
+            _1.IsHitTestVisible = false;
+            _2.IsHitTestVisible = false;
+            _3.IsHitTestVisible = false;
+            _4.IsHitTestVisible = false;
+            _5.IsHitTestVisible = false;
+            _6.IsHitTestVisible = false;
+            _7.IsHitTestVisible = false;
+            _8.IsHitTestVisible = false;
+            _9.IsHitTestVisible = false;
+        }
+
+        private void  make_move(Button box)
+        {
+            int x, y;
+            string tag = box.Tag.ToString();
+            x = int.Parse (tag.Substring(0,1));
+            y = int.Parse(tag.Substring(1, 1));
+            int side = logic.side;
+            if (logic.Place(x, y))
+            {
+                if (side == 1)
+                {
+                    krest((Button)box);
+                    
+                }
+                else
+                {
+                    nolik((Button)box);
+                    
+                }
+                if(logic.finish != "play")
+                {
+                    konec();
+                }
+            }
+        }
+        private void konec()
+        {
+            switch (logic.finish) 
+            {
+                case "Winx": Nadpis.Text = "Крестики победили\n Конец игры"; return;
+                case "Wino": Nadpis.Text = "Нолики победили\n Конец игры"; return;
+                case "draw": Nadpis.Text = "Ничья\n Конец игры"; return;
+                default: return;
+            }
+        }
+        private void krest(Button box)
+        {
+            var a = new Uri(System.IO.Path.GetFullPath(@"..\..\Images\krest.jpg"), UriKind.Relative);
+            ImageBrush krest = new ImageBrush(new BitmapImage(a));
+            box.Background = krest;
+        }
+        private void nolik(Button box)
+        {
+            var a = new Uri(System.IO.Path.GetFullPath(@"..\..\Images\Nolik.jpg"), UriKind.Relative);
+            ImageBrush nolik = new ImageBrush(new BitmapImage(a));
+            box.Background = nolik;
+        }
+        private void _1_Click(object sender, RoutedEventArgs e)
+        {
+            make_move((Button) sender);
+        }
+
+        private void _2_Click(object sender, RoutedEventArgs e)
+        {
+            make_move((Button)sender);
+        }
+
+        private void _3_Click(object sender, RoutedEventArgs e)
+        {
+            make_move((Button)sender);
+        }
+
+        private void _4_Click(object sender, RoutedEventArgs e)
+        {
+            make_move((Button)sender);
+        }
+
+        private void _5_Click(object sender, RoutedEventArgs e)
+        {
+            make_move((Button)sender);
+        }
+
+        private void _6_Click(object sender, RoutedEventArgs e)
+        {
+            make_move((Button)sender);
+        }
+
+        private void _7_Click(object sender, RoutedEventArgs e)
+        {
+            make_move((Button)sender);
+        }
+
+        private void _8_Click(object sender, RoutedEventArgs e)
+        {
+            make_move((Button)sender);
+        }
+
+        private void _9_Click(object sender, RoutedEventArgs e)
+        {
+            make_move((Button)sender);
+        }
+
+        private void Основа_Click(object sender, RoutedEventArgs e)
+        {
+            Nadpis.Text = null;
+            logic.Init();
+            
+            _1.Background = null;
+            _2.Background = null;
+            _3.Background = null;
+            _4.Background = null;
+            _5.Background = null;
+            _6.Background = null;
+            _7.Background = null;
+            _8.Background = null;
+            _9.Background = null;
+
+            _1.IsHitTestVisible = true;
+            _2.IsHitTestVisible = true;
+            _3.IsHitTestVisible = true;
+            _4.IsHitTestVisible = true;
+            _5.IsHitTestVisible = true;
+            _6.IsHitTestVisible = true;
+            _7.IsHitTestVisible = true;
+            _8.IsHitTestVisible = true;
+            _9.IsHitTestVisible = true;
+        }
+
+        private void конец_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+    }
+}
